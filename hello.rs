087163a290a9af662d916e2a2ae0eb0808d053b4
guy@xscode.com <guy@xscode.com>
2020-07-29 13:22:15 +0300

    hello rs added

---
+++ hello.rs
@@ -0,0 +1,8 @@
+// YAY we have some rust code
+
+fn main() {
+    // Statements here are executed when the compiled binary is called
+
+    // Print text to the console
+    println!("Hello World!");
+}
